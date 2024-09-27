3fb00718d59e1a3dec053c27963bff99bce439de
Rayyan Shaikh <rayyanshaikh2003@gmail.com>
2024-09-27 16:03:30 -0400

    Initial navigation bar display

---
+++ src/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const navigation = ['Description', 'Languages', 'Experience', 'Certifications', 'Projects']
+
+    return (
+        <nav className="navbar">
+            <ul className='nav-links'>
+                {navigation.map((link, index) => (
+                    <li key={index} className='underline-hover'><a href={link}>{link}</a></li>
+                ))}
+            </ul>
+        
+            <ul className='nav-links'>
+                <li className='nav-contact'>
+                    <a href='mailto:rayyan.shaikh@mail.utoronto.ca'>Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
