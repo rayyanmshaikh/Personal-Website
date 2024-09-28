3c64d45915ab1dc0df1c4049c6e477004b9244f7
Rayyan Shaikh <rayyanshaikh2003@gmail.com>
2024-09-28 13:54:23 -0400

    Implement sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Desc from './Description'
 import Nav from './Navbar'
+import Sidebar from './Sidebar'
 import './App.css'
 
 function App() {
   return (
     <>
       <Nav />
+      <Sidebar />
       <Desc />
     </>
   )
