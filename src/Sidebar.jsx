534416b7bddefe367e75523e6bf59e35883f26ff
Rayyan Shaikh <rayyanshaikh2003@gmail.com>
2024-09-28 13:53:46 -0400

    Implement sidebar

---
+++ src/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css';
+import github from './assets/github-logo.svg'
+import linkedin from './assets/linkedin-logo.svg'
+import resume from './assets/resume-logo.svg'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <a href='https://www.linkedin.com/in/rayyan-m-shaikh/'><img src={linkedin}/></a>
+      <a href='https://github.com/rayyanmshaikh'><img src={github}/></a>
+      <a href='https://drive.google.com/file/d/1PFC8FInwBTAkt8-L53S1oqUR8VLiByob/view?usp=sharing'><img src={resume}/></a>
+    </div>
+  );
+}
+
+export default Sidebar;
