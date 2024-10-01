7c4b4ec6648d4a92aff16d545c5c3e1906d48e24
Rayyan Shaikh <rayyanshaikh2003@gmail.com>
2024-10-01 15:14:43 -0400

    Add tech stack section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Desc from './Description'
 import Nav from './Navbar'
 import Sidebar from './Sidebar'
+import TechStack from './TechStack'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Sidebar />
       <Desc />
+      <TechStack />
     </>
   )
 }
