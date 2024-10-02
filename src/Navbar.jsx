a571bf320857b39a31b72e3e71c919d594f862f9
Rayyan Shaikh <rayyanshaikh2003@gmail.com>
2024-10-02 10:31:23 -0400

    Update animation

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import { Zoom } from "react-awesome-reveal"
 
 const Navbar = () => {
     const navigation = ['Tech Stack', 'Projects']
@@ -11,12 +12,14 @@ const Navbar = () => {
                     <li key={index} className='underline-hover'><a href={link}>{link}</a></li>
                 ))}
             </ul>
-        
-            <ul className='nav-links'>
-                <li className='nav-contact'>
-                    <a href='mailto:rayyan.shaikh@mail.utoronto.ca'>Contact</a>
-                </li>
-            </ul>
+            
+            <Zoom>
+                <ul className='nav-links'>
+                    <li className='nav-contact'>
+                        <a href='mailto:rayyan.shaikh@mail.utoronto.ca'>Contact</a>
+                    </li>
+                </ul>
+            </Zoom>
         </nav>
     );
 };
--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -7,6 +7,7 @@ import Cpp from './assets/Cpp.png'
 import Reactjs from './assets/Reactjs.png'
 import Nodejs from './assets/Nodejs.png'
 import SQL from './assets/SQL.png'
+import { Bounce } from "react-awesome-reveal"
 
 const techImages = {
     'Python': Python,
@@ -23,20 +24,15 @@ function TechStack() {
 
     return (
         <div>
-            <h1>My Stack</h1>
+            <Bounce><h1>My <span className='text-green'>Stack</span></h1></Bounce>
 
             <div class="technology__list">
-
-                    {tech.map((tech_name) => (
-                        <div class="technology">
-                            <figure class="technology__img--wrapper">
-                                <img src={techImages[tech_name]} class="technology__img" />
-                            </figure>
-
-                            <span class="technology__name">{tech_name}</span>
-                        </div>
-                    ))}
-
+                {tech.map((tech_name) => (
+                    <div class="technology">
+                        <figure class="technology__img--wrapper"><img src={techImages[tech_name]} class="technology__img" /></figure>
+                        <span class="technology__name">{tech_name}</span>
+                    </div>
+                ))}
             </div>
         </div>
     )
